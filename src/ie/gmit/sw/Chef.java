6e64a599b8b4850d5d8c864b81d8956e51cfa49f
Will Hogan <willhogan11@hotmail.com>
2016-03-03 01:01:19 +0000

    Fixed issue surrounding Facebook API login requirements by reconfiguring and customising the webapps folder in tomcat

---
+++ src/ie/gmit/sw/Chef.java
@@ -0,0 +1,27 @@
+package ie.gmit.sw;
+
+public class Chef {
+	
+	private int id;
+	private String firstName;
+	private String surname;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+}
---
+++ src/ie/gmit/sw/ChefDatabaseEntry.java
@@ -0,0 +1,39 @@
+package ie.gmit.sw;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ChefDatabaseEntry
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/ChefDatabaseEntry" })
+public class ChefDatabaseEntry extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ChefDatabaseEntry() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		 String name = request.getParameter("fbLogin");
+	}
+
+}
